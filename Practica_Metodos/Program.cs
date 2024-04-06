9254653d8a4c526b35adb62abc152635db1344b3
fA3DKAN <103541453+fAEDKAN@users.noreply.github.com>
2024-04-06 18:21:42 -0300

    Agregar archivos de proyecto.

---
+++ Practica_Metodos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practica_Metodos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            sumaNumeros(8, 5);
+        }
+
+        static void sumaNumeros(int num1, int num2)
+        {
+            Console.WriteLine($"La suma de los números es: {num1 + num2}");
+        }
+
+    }
+}
